b0cabac9798bd22c3d9b68eecda3127e46546feb
Aurora Xu <78364375+aurora33x@users.noreply.github.com>
2024-07-02 14:51:20 +1200

    change

--- backend/src/data/user-dao.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-import User from "../model/user";
-
-const secret = 'secret123';
-
-export function getUser(token) {
-  const user = jwt.verify(token, secret);
-  return User.findById(user.id);
-}
