433fb8c7c194511d117c680fa725ff7d9a2c2af8
Aurora Xu <78364375+aurora33x@users.noreply.github.com>
2024-07-03 16:41:29 +1200

    Update LoginPage.js

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -3,7 +3,6 @@ import { Button, Card, Col, Input, Layout, message, Row, Typography } from "antd
 import axios from "axios";
 import Cookies from 'js-cookie';
 import React, { useContext, useState } from "react";
-import { GoogleReCaptcha } from "react-google-recaptcha-v3";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import UserContext from "../components/UserContext";
@@ -18,85 +17,79 @@ function LoginPage() {
 
   const user = useContext(UserContext);
   const base_url = process.env.REACT_APP_NODE_ENV === 'development' ? process.env.REACT_APP_LOCAL_BASE_URL : process.env.REACT_APP_SERVER_BASE_URL;
+
   function login() {
     const data = { username, password };
-    if (!isHuman) {
-      return message.error("Your interaction doesn't look like a human");
-    } else {
-      if (!username) return message.error('Please enter username');
-      if (!password) return message.error('Please enter password');
-  
-      axios.post(`${base_url}/auth/login`, data)
-        .then((response) => {
-          console.log(response.data);
-          user.setToken(Cookies.get('token')); // Assuming token is returned in response
+    if (!username) return message.error('Please enter username');
+    if (!password) return message.error('Please enter password');
+
+    axios.post(`${base_url}/auth/login`, data)
+      .then((response) => {
+        const { token } = response.data; // Assuming token is returned in response
+        if (token) {
+          Cookies.set('token', token); // Store token securely
+          user.setToken(token); // Update user context with token
           navigate("/");
-        })
-        .catch((error) => {
-          if (error.response && error.response.status === 403) {
-            message.error('Invalid username or password');
-            console.log(error.response);
-          } else {
-            message.error('Login failed. Please try again later.');
-          }
-        });
-    }
-  }
-  
-  const [isHuman, setIsHuman] = useState(false);
-  function handleOnVerify() {
-    setIsHuman(true);
+        } else {
+          message.error('Login failed. No token received.');
+        }
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 403) {
+          message.error('Invalid username or password');
+          console.log(error.response);
+        } else {
+          message.error('Login failed. Please try again later.');
+        }
+      });
   }
 
   return (
-    <>
-      <GoogleReCaptcha onVerify={handleOnVerify} />
-      <Layout>
-        <Navbar />
-        <Row>
-          <Col span={9} offset={6}>
-            <Card
-              bordered={false}
-              className="login-page"
-              title={<Title level={3}>Log In</Title>}
+    <Layout>
+      <Navbar />
+      <Row>
+        <Col span={9} offset={6}>
+          <Card
+            bordered={false}
+            className="login-page"
+            title={<Title level={3}>Log In</Title>}
+          >
+            <Input
+              size="large"
+              placeholder="Username"
+              prefix={<UserOutlined />}
+              style={{ width: "80%" }}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+            <br />
+            <br />
+            <Input
+              type="password"
+              size="large"
+              placeholder="Password"
+              prefix={<LockOutlined />}
+              style={{ width: "80%" }}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <br />
+            <br />
+            <br />
+            <Button
+              type="primary"
+              size="large"
+              id="login-button"
+              onClick={login}
             >
-              <Input
-                size="large"
-                placeholder="Username"
-                prefix={<UserOutlined />}
-                style={{ width: "80%" }}
-                onChange={(e) => {
-                  setUsername(e.target.value);
-                }}
-              />
-              <br />
-              <br />
-              <Input
-                type="password"
-                size="large"
-                placeholder="Password"
-                prefix={<LockOutlined />}
-                style={{ width: "80%" }}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
-              <br />
-              <br />
-              <br />
-              <Button
-                type="primary"
-                size="large"
-                id="login-button"
-                onClick={login}
-              >
-                LOGIN
-              </Button>
-            </Card>
-          </Col>
-        </Row>
-      </Layout>
-    </>
+              LOGIN
+            </Button>
+          </Card>
+        </Col>
+      </Row>
+    </Layout>
   );
 }
 
